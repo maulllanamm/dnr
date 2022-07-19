a0cbec2ce640c787b7ae92a52f1d08a9499bd6cb
maulllanamm <maulllanamuhammad@gmail.com>
2022-07-19 16:21:31 +0700

    footer

--- application/views/landingpage/view.php
+++ application/views/landingpage/view.php
@@ -17,70 +17,142 @@
 </div>
 <!-- HERO END -->
 
-<!-- PROPERTIES START -->
-<div class="container">
-   <div class="properties">
-      <h1 class="title">Our Properties</h1>
-      <h3 class="subtitle">Ongoing project</h3>
-      <div class="row">
-         <div class="col-sm-4">
+    <!-- PROPERTIES START -->
+    <div class="container">
+      <div class="properties">
+        <h1 class="title">Our Properties</h1>
+        <h3 class="subtitle">Ongoing project</h3>
+        <div class="row">
+          <div class="col-sm-4">
             <div class="box-img">
-               <img src="assets/img/Mask group.png" class="proper-img" alt="">
-               <span class="title-proper">Pesona Nirwana</span>
-               <p class="subtitle-proper">Sentul, Bogor</p>
+              <img src="assets/img/Mask group.png" class="proper-img" alt="" />
+              <span class="title-proper">Pesona Nirwana</span>
+              <p class="subtitle-proper">Sentul, Bogor</p>
             </div>
-         </div>
-         <div class="col-sm-4">
-            <div class="box-img">
-               <img src="assets/img/Mask group.png" class="proper-img" alt="">
+          </div>
+          <div class="col-sm-4">
+            <div class="box-img coming-soon">
+              <img src="assets/img/Mask group.png" class="proper-img" alt="" />
             </div>
-         </div>
-         <div class="col-sm-4">
-            <div class="box-img">
-               <img src="assets/img/Mask group.png" class="proper-img" alt="">
+          </div>
+          <div class="col-sm-4">
+            <div class="box-img coming-soon">
+              <img src="assets/img/Mask group.png" class="proper-img" alt="" />
             </div>
-         </div>
-      </div>
-      <h3 class="subtitle">Finished Project</h3>
-      <div class="row">
-         <div class="col-sm-3">
+          </div>
+        </div>
+        <h3 class="subtitle">Finished Project</h3>
+        <div class="row">
+          <div class="col-sm-3">
             <div class="box-img-2">
-               <img src="assets/img/img4.png" class="proper-img-2" alt="">
-               <span class="title-proper-2">Ciomas permai</span>
-               <p class="subtitle-proper-2">Ciomas, Bogor</p>
+              <img src="assets/img/img4.png" class="proper-img-2" alt="" />
+              <span class="title-proper-2">Ciomas permai</span>
+              <p class="subtitle-proper-2">Ciomas, Bogor</p>
             </div>
-         </div>
-         <div class="col-sm-3">
+          </div>
+          <div class="col-sm-3">
             <div class="box-img-2">
-               <img src="assets/img/img5.png" class="proper-img-2" alt="">
-               <span class="title-proper-2">Linea house</span>
-               <p class="subtitle-proper-2">Empang, Bogor</p>
-               <span class="unggulan">unggulan</span>
+              <img src="assets/img/img5.png" class="proper-img-2" alt="" />
+              <span class="title-proper-2">Linea house</span>
+              <p class="subtitle-proper-2">Empang, Bogor</p>
+              <span class="unggulan">unggulan</span>
             </div>
-         </div>
-         <div class="col-sm-3">
+          </div>
+          <div class="col-sm-3">
             <div class="box-img-2">
-               <img src="assets/img/img6.png" class="proper-img-2" alt="">
-               <span class="title-proper-2">Vila anggrek</span>
-               <p class="subtitle-proper-2">Puncak, Bogor</p>
-               <span class="unggulan">unggulan</span>
+              <img src="assets/img/img6.png" class="proper-img-2" alt="" />
+              <span class="title-proper-2">Vila anggrek</span>
+              <p class="subtitle-proper-2">Puncak, Bogor</p>
+              <span class="unggulan">unggulan</span>
             </div>
-         </div>
-         <div class="col-sm-3">
+          </div>
+          <div class="col-sm-3">
             <div class="box-img-2">
-               <img src="assets/img/img8.png" class="proper-img-2" alt="">
-               <span class="title-proper-2">BCI</span>
-               <p class="subtitle-proper-2">Cikereteg, Bogor</p>
+              <img src="assets/img/img8.png" class="proper-img-2" alt="" />
+              <span class="title-proper-2">BCI</span>
+              <p class="subtitle-proper-2">Cikereteg, Bogor</p>
             </div>
-         </div>
-      </div>
-      <div class="row">
-         <div class="col-sm-12">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-12">
             <div class="text-center my-5">
-               <button class="btn-lainnya">Lihat lainnya</button>
+              <button class="btn-lainnya">Lihat lainnya</button>
             </div>
-         </div>
+          </div>
+        </div>
       </div>
-   </div>
-</div>
-<!-- PROPERTIES END -->
+    </div>
+    <!-- PROPERTIES END -->
+
+    <!-- Footer Start -->
+
+    <footer>
+      <div class="bg-footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-3">
+              <img src="assets/img/logo DNR copy 1.png" class="img-footer" alt="" />
+              <p class="p-footer">
+                Mauris egestas turpis. Vitae pulvinar lobortis vel ut at. Sed
+                facilisis vestibulum, sed bibendum. Vitae, tristique suspendisse
+              </p>
+              <div class="icon">
+                <span><i class="bi bi-facebook"></i></span>
+                <span><i class="bi bi-instagram"></i></span>
+              </div>
+            </div>
+            <div class="col-sm-2 offset-1">
+              <div class="box">
+                <ul>
+                  <a href="">Tentang kami</a>
+                </ul>
+                <ul>
+                  <a href="">Beranda</a>
+                </ul>
+                <ul>
+                  <a href="">Layanan kami</a>
+                </ul>
+                <ul>
+                  <a href="">Kontak</a>
+                </ul>
+              </div>
+            </div>
+            <div class="col-sm-2">
+              <div class="box">
+                <ul>
+                  <a href="">Layanan</a>
+                </ul>
+                <ul>
+                  <a href="">Invest</a>
+                </ul>
+                <ul>
+                  <a href="">Rent</a>
+                </ul>
+                <ul>
+                  <a href="">Estate</a>
+                </ul>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <div class="box">
+                <ul>
+                  <a href="">Getting touch</a>
+                </ul>
+                <ul>
+                  <a href="">Support@dutagrahan.id</a>
+                </ul>
+                <ul>
+                  <a href="">021 - 8493 - 6386</a>
+                </ul>
+                <ul>
+                  <a href="">dutagrahaNirwana, Bogor</a>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+<!-- Footer End -->
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,6 +8,10 @@
 
    <!-- Bootstrap CSS -->
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+   <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
+    />
    <link rel="stylesheet" href="<?= base_url('/assets/css/style.css'); ?>">
 
    <title>Hello, world!</title>
